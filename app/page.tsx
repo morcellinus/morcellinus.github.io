d6cc0f8c7ef89da61c8bc1683fe09b122bd76a64
morcellinus <jsjs4595@gmail.com>
2025-08-15 14:02:36 +0900

    minor details for publications tab

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,16 @@ export default function HomePage() {
     <section className="space-y-6">
       <div>
         <h1 className="text-3xl font-semibold tracking-tight">Hi, I’m Jinmo Lee 👋</h1>
-        <p className="mt-3 text-neutral-700 max-w-2xl">
-          Currently a PhD student at <b>KAIST School of Electrical Engineering (25.08-)</b>, advised by <b>Prof. Jaemin Yoo</b>. <br />
-          B.S. in Statistics, Sunkyunkwan University (Summa Cum Laude, GPA : 4.38/4.50) <br />
-          M.S. in Applied Artificial Intelligence, advised by Prof. Eunil Park (GPA: 4.30/4.50) <br />
+        <p className="mt-3 text-neutral-1200 max-w-2xl">
+          Currently a PhD student at <b>KAIST School of Electrical Engineering (25.08-)</b>, advised by <b>Prof. Jaemin Yoo</b>.
+        </p>
+        <p>
+          B.S. in Statistics, Sunkyunkwan University (Summa Cum Laude, GPA : 4.38/4.50)
+        </p>
+        <p>
+          M.S. in Applied Artificial Intelligence, advised by Prof. Eunil Park (GPA: 4.30/4.50)
+        </p>
+        <p>
           Previously worked at KB Kookmin Card (22.11-23.07) and NH Investments & Securities (23.08-25.07) as a data scientist.
         </p>
       </div>
--- app/research/page.tsx
+++ app/research/page.tsx
@@ -17,14 +17,14 @@ export default function ResearchPage() {
           <h3 className="font-semibold">2025</h3>
           {/* [J2] 2025 */}
           <li className="border rounded-xl p-4">
-            <a className="font-medium" href = "https://journalofbigdata.springeropen.com/articles/10.1186/s40537-025-01203-9">[J2] We know who wins: graph-oriented approaches of passing networks for predictive football match outcomes</a>
+            <a className="font-medium" href = "https://journalofbigdata.springeropen.com/articles/10.1186/s40537-025-01203-9" target="_blank">[J2] We know who wins: graph-oriented approaches of passing networks for predictive football match outcomes</a>
             <div className="text-sm text-neutral-600"><b>J Lee</b>, E Park, AP del Pobil. <i>Journal of Big Data</i> 12(1), 147 (2025).</div>
           </li>
 
           {/* [J1] 2023 */}
           <h3 className="font-semibold">2023</h3>
           <li className="border rounded-xl p-4">
-            <a className="font-medium" href="https://www.sciencedirect.com/science/article/abs/pii/S0736585323000655">[J1] D-HRSP: Dataset of helpful reviews for service providers</a>
+            <a className="font-medium" href="https://www.sciencedirect.com/science/article/abs/pii/S0736585323000655" target="_blank">[J1] D-HRSP: Dataset of helpful reviews for service providers</a>
             <div className="text-sm text-neutral-600"><b>J Lee</b>, E Park. <i>Telematics and Informatics</i> 82, 102001 (2023).</div>
             
           </li>
